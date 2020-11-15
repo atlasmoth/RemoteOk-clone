cf6abadccd44ed075b06582c3e12a53ed32d9ffa
atlasmoth <ebuka422@gmail.com>
2020-11-15 12:53:09 +0100

    preview works now

--- public/script/remotely.js
+++ public/script/remotely.js
@@ -1,7 +1,39 @@
-const inputs = document.querySelectorAll("label ~ [name]");
+const inputs = Array.from(document.querySelectorAll("label ~ [name]"));
+const data = Array.from(document.querySelectorAll("[data-information]"));
 
-inputs.forEach((input) => {
-  input.addEventListener("change", (e) => {
-    console.log(e.target.value);
+const props = inputs.filter((input) => {
+  let val = data.find((node) => {
+    return node.getAttribute("data-information") === input.name;
   });
+  if (val) return val;
+});
+
+props.forEach((prop) => {
+  if (prop.type !== "file") {
+    prop.addEventListener("keyup", (e) => {
+      let val = data.filter((node) => {
+        return node.getAttribute("data-information") === prop.name;
+      });
+      val.forEach((source) => {
+        if (source.classList.contains("tags")) {
+          source.innerHTML = e.target.value.split(",").reduce((acc, curr) => {
+            return (acc += `<button>${curr}</button>`);
+          }, ``);
+        } else {
+          source.innerText = e.target.value;
+        }
+      });
+    });
+  } else {
+    prop.addEventListener("change", (e) => {
+      const [file] = e.target.files;
+      if (file.type.startsWith("image")) {
+        const imageUrl = URL.createObjectURL(file);
+        let val = data.filter((node) => {
+          return node.getAttribute("data-information") === prop.name;
+        });
+        val.forEach((img) => (img.src = imageUrl));
+      }
+    });
+  }
 });
